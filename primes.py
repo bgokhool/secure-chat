c24ec0efb2e201fbb04e6677c831ca5db8b2b6a3
Bhuwan Gokhool <gokhoolb.union.edu>
2020-03-04 17:55:29 -0500

    Generate dict of large primes for later use

--- primes.py
+++ primes.py
@@ -35,3 +35,19 @@ if __name__ == "__main__":
     print()
     print(g)
     print(r.fast_exp_w_mod(g,p-1,p))
+
+mydict = {}
+for n in range(512, 1088, 64):
+    dsa = DSA.generate(n)
+    p_g_tuple = (dsa.p, dsa.g)
+    mydict[str(n)] = p_g_tuple
+
+# print(mydict["512"])
+# print(mydict["576"])
+# print(mydict["640"])
+# print(mydict["704"])
+# print(mydict["768"])
+# print(mydict["832"])
+# print(mydict["896"])
+# print(mydict["960"])
+# print(mydict["1024"])
